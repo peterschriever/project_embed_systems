719118c871eb5dee9f6409cb8bff02dd058f5836
Peter Schriever <peterzen72@gmail.com>
2016-10-27 12:00:06 +0200

    Initial setup of dashboard and ajax_api project apps

---
+++ userpanel/ajax_api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from ajax_api import json_views
+
+urlpatterns = [
+    # @TODO: actually create api, this is just an example
+    # /api/v1/get-temperature-data
+    url(r'^get-temperature-data$', json_views.getTemperatureData, \
+        name="ajx_getTempData")
+]
